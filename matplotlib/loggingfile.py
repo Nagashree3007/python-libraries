c31b320c2485986538b06378b3e67c0adfa08d0e
Nagashree3007 <crnagashree8@gmail.com>
2024-08-04 08:53:03 +0530

    [Nagashree] Add: logging file

---
+++ matplotlib/loggingfile.py
@@ -0,0 +1,22 @@
+import logging
+
+def logger_init(name):
+    logger = logging.getLogger(name)
+    # configuring our specific logger
+    # setting the log level for the logger
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.setLevel(logging.DEBUG)
+    # creating a formatter
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    file_handler = logging.FileHandler('matplotlib.log')
+    # adding the formatting to the file handler
+    file_handler.setFormatter(formatter)
+    # to display the result in the console
+    stream_handler = logging.StreamHandler()
+    # adding the formatting to the stream handler
+    stream_handler.setFormatter(formatter)
+    # adding file handler and stream_handler to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
